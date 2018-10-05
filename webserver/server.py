cccd51c2cd0148fda7485aecafce293a83e6e9c8
U-FRIENDLY_NAME\eric <eric@mink.li>
2018-10-06 01:31:53 +0200

    added database environment variables

--- webserver/server.py
+++ webserver/server.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import os
 from flask import Flask
 from flask import request, send_from_directory, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+# set up server directory for web
 STATIC_DIR = 'static'
 STATIC_DIR_WITH_SLASH = "/{}".format(STATIC_DIR)
+# set up global server variable and global database variable, based on environment variables
 FLASK_SERVER = Flask(__name__, static_url_path=STATIC_DIR_WITH_SLASH, static_folder=STATIC_DIR)
+DB_URL = os.environ.get("RUNTIME_POSTGRES_DB_SERVER")
+DB_PORT = os.environ.get("RUNTIME_POSTGRES_DB_PORT")
+DB_NAME = os.environ.get("RUNTIME_POSTGRES_DB_NAME")
+DB_USER = os.environ.get("RUNTIME_POSTGRES_DB_USER")
+DB_PW = os.environ.get("RUNTIME_POSTGRES_DB_PW")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://{0}:{1}@{2}:{3}/{4}'.format(DB_USER, DB_PW, DB_URL, DB_PORT, DB_NAME)
+DB = SQLAlchemy(FLASK_SERVER)
 
 def main():
     FLASK_SERVER.run('0.0.0.0', port=80)
