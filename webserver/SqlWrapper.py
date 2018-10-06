852d91ac0cb63814303f1fbf8297e70860e0bce9
U-FRIENDLY_NAME\eric <eric@mink.li>
2018-10-06 03:24:58 +0200

    debugging in production, yey

--- webserver/SqlWrapper.py
+++ webserver/SqlWrapper.py
@@ -14,7 +14,7 @@ assistant_nethz is limited to 32 characters
 def MakeAssistant (assistant_nethz, dbname, user, password, host, port):
         conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
         cur = conn.cursor()     
-        cur.execute("DO $$ BEGIN PERFORM MakeAssistant(%s); END; $$;", (assistant_nethz))       
+        cur.execute("DO $$ BEGIN PERFORM MakeAssistant(%s); END; $$;", (assistant_nethz,))       
         conn.commit()
         cur.close()
         conn.close()
