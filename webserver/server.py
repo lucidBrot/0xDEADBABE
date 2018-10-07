5c2e8a0c32d2480e13fe6a367368dc51de53bc30
U-FRIENDLY_NAME\eric <eric@mink.li>
2018-10-07 04:55:31 +0200

    jasper run plzs

--- webserver/server.py
+++ webserver/server.py
@@ -120,9 +120,11 @@ def userLogin():
 @FLASK_SERVER.route('/submitRatings', methods=["POST"])
 def submitRatings():
     ratingsDictListJSON = request.form.get('ratings')
+    print("RatingsDictListJSON: "+str(ratingsDictListJSON)
     # ratingsDictJSON contains keys and values as a dictionary. And that repeated, in a list.
     ratingsDictList = json.loads(ratingsDictListJSON)
     ratingsList = ratingsDictList.map(lambda dictionary: tuple(dictionary.values()))
+    print("ratings list: "+str(ratingsList))
 
     user_nethz = session["nethz_cookie"]
     retStr = "Got ratings: {}".format(str(ratingsList))
